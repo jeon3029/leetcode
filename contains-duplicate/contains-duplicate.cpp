5701a0cd1b05c4ca4567c75b9295c4214464e249
jeon3029 <jeon3029@naver.com>
2021-06-07 12:00:32 +0900

    Time: 60 ms (5.52%), Space: 20.2 MB (48.59%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(auto i:nums){
+            if(m.find(i)!=m.end()){
+                return true;
+            }
+            m[i]=1;
+        }
+        return false;
+    }
+};
